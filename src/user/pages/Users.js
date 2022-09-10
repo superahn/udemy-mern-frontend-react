9de9097b6a185b9c9c50e3c8326759b9b5d9aaf5
Sung Ahn <superahn@gmail.com>
2022-09-11 01:45:06 +1000

    Getting users with Http GET request. Added ErrorModal and LoadingSpinner to show more details.

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Sung Ahn",
-      image: "https://avatars.githubusercontent.com/u/941784?s=400&v=4 ",
-      places: 3,
-    },
-  ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-  return <UsersList items={USERS} />;
+  // for being called once
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5500/api/users");
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest(); // To use async function. Because you can't use async directly to useEffect()
+  }, []);
+
+  const errorClearHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorClearHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
